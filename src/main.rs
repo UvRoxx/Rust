d9706040a280e742216cddeb91d7238040128995
Utkarsh Varma <utkarsh@webisoft.com>
2021-10-07 09:04:49 -0400

    added main

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+// RUST CODE & NOTES -- Utkarsh Varma
+
+// Rust uses snake_case
+// In rust we use mod to specify import
+// The process is called "attach" here
+
+// mod print;
+// mod vars;
+// mod types;
+// mod strings;
+// mod tuples;
+// mod arrays;
+// mod vectors;
+mod conditionals;
+
+fn main() {
+    // This is the syntax for accessing the lib and :: to open a given pub function from that lib
+    conditionals::run();
+}
